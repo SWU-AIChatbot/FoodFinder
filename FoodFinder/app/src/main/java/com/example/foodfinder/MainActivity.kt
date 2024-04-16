83edf923fab80ed6193cbeb9d28d200bf943f501
HJ2828 <tjgmlwo0228@gmail.com>
2024-04-17 04:02:17 +0900

    [FEAT] MenuActivity 생성 및 cameraX 이용한 사진 찍기 구현, MainActivity에서 text 버튼 연결 완료(MenuActivity로 연결)

--- FoodFinder/app/src/main/java/com/example/foodfinder/MainActivity.kt
+++ FoodFinder/app/src/main/java/com/example/foodfinder/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.foodfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menuIv = findViewById<ImageView>(R.id.menu_iv)
+
+        menuIv.setOnClickListener {   // MenuActiviy로 전환
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
